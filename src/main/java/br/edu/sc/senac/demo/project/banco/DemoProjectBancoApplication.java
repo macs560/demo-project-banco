571a186ef29a6169aecb84d758d52335de05c8a5
macs560 <62939195+macs560@users.noreply.github.com>
2020-05-01 15:48:23 -0300

    Add files via upload

---
+++ src/main/java/br/edu/sc/senac/demo/project/banco/DemoProjectBancoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.sc.senac.demo.project.banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectBancoApplication.class, args);
+	}
+
+}
